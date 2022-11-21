049cabe4da243ac0cf1e1a2f4891e8ab1b201ed7
JimiUrsin <32343914+JimiUrsin@users.noreply.github.com>
2022-11-21 20:30:13 +0200

    3.3 done

--- index.js
+++ index.js
@@ -6,26 +6,36 @@ let persons = [
         "id": 1,
         "name": "Arto Hellas", 
         "number": "040-123456"
-      },
-      { 
+    },
+    { 
         "id": 2,
         "name": "Ada Lovelace", 
         "number": "39-44-5323523"
-      },
-      { 
+    },
+    { 
         "id": 3,
         "name": "Dan Abramov", 
         "number": "12-43-234345"
-      },
-      { 
+    },
+    { 
         "id": 4,
         "name": "Mary Poppendieck", 
         "number": "39-23-6423122"
-      }
+    }
   ]
   
-  app.get('/api/persons', (request, response) => {
+  app.get('/api/persons', (_, response) => {
     response.json(persons)
+  })
+
+  app.get('/api/persons/:id', (request, response) => {
+    const person = persons.find(person => person.id === request.params.id)
+
+    if (person === undefined) {
+        response.status(404).send(`No person was found with given id`)
+    }
+
+    response.json(person)
   })
 
   app.get('/info', (_, response) => {
